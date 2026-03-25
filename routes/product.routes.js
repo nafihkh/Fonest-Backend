7f8511ce158c70901f719342e38911968ad64b0c
nafihkh <muhammednafih.k.hkottankodan@gmail.com>
2026-03-25 17:32:29 +0530

    product routs

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,15 +1,23 @@
 const router = require("express").Router();
 const upload = require("../middleware/uploadMemory");
 const product = require("../controllers/product.controller");
+const auth = require("../middleware/auth");
 
 // If you have auth middleware, add it:
 // const { verifyAccessToken } = require("../middleware/auth");
 
-router.post("/products", upload.array("images", 6), product.createProduct);
-router.get("/products", product.listProducts);
-router.get("/products/stats", product.productStats);
-router.patch("/products/:id/publish", product.publishProduct);
-router.patch("/products/bulk-action", product.bulkActionProducts);
-router.get("/search-suggestions", product.searchProductSuggestions);
-router.patch("/low-stock-threshold", product.applyLowStockThresholdToAllProducts);
+router.post("/products", auth(["admin"]), upload.array("images", 6), product.createProduct);
+router.get("/products", auth(["admin"]), product.listProducts);
+router.get("/products/stats", auth(["admin"]), product.productStats);
+router.patch("/products/:id/publish", auth(["admin"]), product.publishProduct);
+router.patch("/products/bulk-action", auth(["admin"]), product.bulkActionProducts);
+router.get("/search-suggestions", auth(["admin"]), product.searchProductSuggestions);
+router.patch("/low-stock-threshold", auth(["admin"]), product.applyLowStockThresholdToAllProducts);
+router.delete("/products/:id", auth(["admin"]), product.deleteProduct);
+
+
+router.get("/store/featured-products", product.listFeaturedProducts);
+router.get("/store/products", product.listStoreProducts);
+router.get("/store/products/:id", product.getStoreProductById);
+router.get("/store/filters", product.getStoreFilters);
 module.exports = router;
